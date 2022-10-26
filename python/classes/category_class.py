3ba980aace5114ad1cfb0c051e03fae68b8721c0
Neil Schroeder <schr1077@umn.edu>
2022-10-26 14:15:47 -0500

    Made some updates

--- python/classes/category_class.py
+++ python/classes/category_class.py
@@ -1,40 +1,63 @@
 
-from matplotlib.pyplot import xcorr
+import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import moment
 
+MIN_EVENTS = 100
 
 class mva_category:
     """ class to describe diphoton mva categories """
 
-    def __init__(self, invmass, weights, is_signal) -> None:
+    def __init__(self, invmass, weights, is_signal, do_sm=True) -> None:
 
         # evaluate range of 68.3% min interval
-        self.range = self.weighted_quantile(invmass, weights, 0.683)
-        if len(self.range) == 0 or sum(weights) == 0:
-            self.s_over_root_b = 0.0
-            self.mean = 0.0
-            self.variance = 0.0
-            self.err_variance = np.inf
-            self.err_mean = np.inf
-            return
         
+        if sum(weights) < MIN_EVENTS:
+            self.set_invalid()
+            return
+
         # select only events in the min interval range
+        self.range = self.weighted_quantile(invmass[is_signal], weights[is_signal], 0.683)
+        #print(self.range)
+        if len(self.range) == 0 or sum(weights) == 0:
+            self.set_invalid()
+            return
+
         mask = np.logical_and(
             self.range[0] <= invmass, invmass <= self.range[1])
 
-        # evaluate statistical properties of events in min interval range
-        self.mean = np.average(invmass[mask], weights=weights[mask])
-        self.variance = np.average(
-            np.power((invmass[mask] - self.mean), 2), weights=weights[mask])
-        self.err_mean = np.sqrt(self.variance)/np.sum(mask)
-        self.err_variance = self.get_err_variance(invmass[mask], weights[mask])
-        signal_weights = weights[np.logical_and(is_signal,mask)]
-        bkg_weights = weights[np.logical_and(np.logical_not(is_signal),mask)]
+        #hist, bins = np.histogram(invmass, bins=20, range = self.range, normed=True, weights=weights)
+        #mids = [(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)]
+
+        #plt.scatter(mids,hist,marker='.', color='b')
+        #plt.scatter(mids, hist1, marker='o', color='r')
+        #plt.show()
+
+        if sum(mask) == 0 or sum(weights[mask]) == 0:
+            self.set_invalid()
+            return
+
+
+        # sorb stuff
+        signal_weights = weights[np.logical_and(is_signal, mask)]
+        bkg_weights = weights[np.logical_and(np.logical_not(is_signal), mask)]
         if len(bkg_weights) > 0:
-            self.s_over_root_b = np.sum(signal_weights)/np.sqrt(np.sum(bkg_weights))
+            self.sig = np.sum(signal_weights)
+            self.bkg = np.sum(bkg_weights)
         else:
-            self.s_over_root_b = 0
+            self.set_invalid()
+            return
+            self.bkg = np.inf
+            self.sig = 0
+
+        if do_sm:
+
+            # evaluate statistical properties of events in min interval range
+            self.mean = np.average(invmass[mask], weights=weights[mask])
+            self.variance = np.average(
+                np.power((invmass[mask] - self.mean), 2), weights=weights[mask])
+            self.err_mean = np.sqrt(self.variance)/np.sum(mask)
+            self.err_variance = self.get_err_variance(
+                invmass[mask], weights[mask])
 
     def get_err_variance(self, x, w):
         """ uncertainty on variance is (m4 - m2^2) / (4 n m2)"""
@@ -49,8 +72,18 @@ class mva_category:
     def weighted_quantile(self, mass, weights, quantiles):
         """ calculates unbinned interval containing target percent of events """
 
-        if len(mass) == 0: return []
+        if len(mass) == 0:
+            return []
         quantiles = [0.5-(quantiles/2), 0.5+(quantiles/2)]
         weighted_quantiles = np.divide(np.subtract(
             np.cumsum(weights), 0.5 * weights), np.sum(weights))
         return np.interp(quantiles, weighted_quantiles, mass)
+
+
+    def set_invalid(self):
+        self.mean = 0.0
+        self.variance = np.inf
+        self.err_variance = np.inf
+        self.err_mean = np.inf
+        self.sig = 0
+        self.bkg = np.inf
